508782a7f9773c8b7ec6101ed3b80f97f4056df7
amatsagu <81469656+amatsagu@users.noreply.github.com>
2026-07-05 21:50:44 +0200

    feat: define bot and snowflake according to v0 docs

---
+++ bot.go
@@ -0,0 +1,164 @@
+package dbl
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+// https://docs.top.gg/api/v0/bots#get-/bots
+type BotQueryOptions struct {
+	Limit  int
+	Offset int
+	Search string
+	Sort   string
+	Fields string
+}
+
+// https://docs.top.gg/api/v0/bots#get-/bots
+type BotQueryResponse struct {
+	Results []Bot `json:"results"`
+	Limit   int   `json:"limit"`
+	Offset  int   `json:"offset"`
+	Count   int   `json:"count"`
+	Total   int   `json:"total"`
+}
+
+// https://docs.top.gg/api/v0/bots#bot-structure
+type Bot struct {
+	ID               Snowflake   `json:"id"`
+	Username         string      `json:"username"`
+	Discriminator    string      `json:"discriminator"`
+	Avatar           string      `json:"avatar,omitempty"`
+	DefAvatar        string      `json:"defAvatar"`
+	Library          string      `json:"lib"`
+	Prefix           string      `json:"prefix"`
+	ShortDescription string      `json:"shortdesc"`
+	LongDescription  string      `json:"longdesc,omitempty"`
+	Tags             []string    `json:"tags,omitzero"`
+	Website          string      `json:"website,omitempty"`
+	Support          string      `json:"support,omitempty"`
+	Github           string      `json:"github,omitempty"`
+	Owners           []Snowflake `json:"owners,omitzero"`
+	Invite           string      `json:"invite,omitempty"`
+	Date             time.Time   `json:"date"`
+	CertifiedBot     bool        `json:"certifiedBot"`
+	Vanity           string      `json:"vanity,omitempty"`
+	MonthlyPoints    int         `json:"monthlyPoints"`
+	Points           int         `json:"points"`
+	DonateBotGuildID Snowflake   `json:"donatebotguildid,omitempty"`
+	ServerCount      int         `json:"server_count,omitempty"`
+	GuildAffiliation []Snowflake `json:"guilds,omitzero"`
+	Shards           []int       `json:"shards,omitzero"`
+}
+
+// https://docs.top.gg/api/v0/bots#get-/bots/bot_id/stats
+// https://docs.top.gg/api/v0/bots#post-/bots/bot_id/stats
+type BotStats struct {
+	ServerCount int   `json:"server_count,omitempty"`
+	Shards      []int `json:"shards,omitzero"`
+	ShardCount  int   `json:"shard_count,omitempty"`
+}
+
+type botVotedResponse struct {
+	Voted int `json:"voted"`
+}
+
+// https://docs.top.gg/api/v0/bots#get-/bots
+func (c *Client) GetBots(options *BotQueryOptions) (*BotQueryResponse, error) {
+	q := url.Values{}
+	if options != nil {
+		if options.Limit > 0 {
+			q.Set("limit", strconv.Itoa(options.Limit))
+		}
+		if options.Offset > 0 {
+			q.Set("offset", strconv.Itoa(options.Offset))
+		}
+		if options.Search != "" {
+			q.Set("search", options.Search)
+		}
+		if options.Sort != "" {
+			q.Set("sort", options.Sort)
+		}
+		if options.Fields != "" {
+			q.Set("fields", options.Fields)
+		}
+	}
+
+	route := "/v0/bots"
+	if len(q) > 0 {
+		route += "?" + q.Encode()
+	}
+
+	b, err := c.request(http.MethodGet, route, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res BotQueryResponse
+	err = json.Unmarshal(b, &res)
+	return &res, err
+}
+
+// https://docs.top.gg/api/v0/bots#get-/bots/bot_id/stats
+func (c *Client) GetBotStats(botID Snowflake) (*BotStats, error) {
+	b, err := c.request(http.MethodGet, fmt.Sprintf("/v0/bots/%s/stats", botID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var stats BotStats
+	err = json.Unmarshal(b, &stats)
+	return &stats, err
+}
+
+// https://docs.top.gg/api/v0/bots#get-/bots
+func (c *Client) GetBot(id Snowflake) (*Bot, error) {
+	b, err := c.request(http.MethodGet, fmt.Sprintf("/v0/bots/%s", id), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var bot Bot
+	err = json.Unmarshal(b, &bot)
+	return &bot, err
+}
+
+// Fetches last 1000 votes of a specific bot.
+// https://docs.top.gg/api/v0/bots#get-%2Fbots%2Fbot_id%2Fvotes
+func (c *Client) GetBotVotes(botID Snowflake) ([]User, error) {
+	b, err := c.request(http.MethodGet, fmt.Sprintf("/v0/bots/%s/votes", botID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []User
+	err = json.Unmarshal(b, &users)
+	return users, err
+}
+
+// Checks if a user has voted for a specific bot.
+// https://docs.top.gg/api/v0/bots#get-/bots/bot_id/check
+func (c *Client) GetBotVote(botID, userID Snowflake) (bool, error) {
+	b, err := c.request(http.MethodGet, fmt.Sprintf("/v0/bots/%s/check?userId=%s", botID, userID), nil)
+	if err != nil {
+		return false, err
+	}
+
+	var res botVotedResponse
+	err = json.Unmarshal(b, &res)
+	if err != nil {
+		return false, err
+	}
+
+	return res.Voted == 1, nil
+}
+
+// https://docs.top.gg/api/v0/bots#post-/bots/bot_id/stats
+func (c *Client) PostBotStats(botID Snowflake, stats BotStats) error {
+	_, err := c.request(http.MethodPost, fmt.Sprintf("/v0/bots/%s/stats", botID), stats)
+	return err
+}
---
+++ snowflake.go
@@ -0,0 +1,50 @@
+package dbl
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Snowflake uint64
+
+func StringToSnowflake(s string) (Snowflake, error) {
+	i, err := strconv.ParseUint(s, 10, 64)
+	return Snowflake(i), err
+}
+
+func EnvToSnowflake(key string) (Snowflake, error) {
+	return StringToSnowflake(os.Getenv(key))
+}
+
+func (s Snowflake) String() string {
+	return strconv.FormatUint(uint64(s), 10)
+}
+
+func (s Snowflake) CreationTimestamp() time.Time {
+	return time.UnixMilli(int64(s>>22 + DISCORD_EPOCH))
+}
+
+func (s Snowflake) MarshalJSON() ([]byte, error) {
+	return json.Marshal(strconv.FormatUint(uint64(s), 10))
+}
+
+func (s *Snowflake) UnmarshalJSON(b []byte) error {
+	if len(b) == 4 && b[0] == 'n' {
+		*s = 0
+		return nil
+	}
+
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+
+	i, err := strconv.ParseUint(string(b), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	*s = Snowflake(i)
+	return nil
+}
