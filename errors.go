b74de6fa9e60d2ab3fa9c5e4f7e99b9aed3a5c8d
amatsagu <81469656+amatsagu@users.noreply.github.com>
2026-07-12 12:25:01 +0200

    style: remove unused error value

--- errors.go
+++ errors.go
@@ -3,9 +3,8 @@ package topgg
 import "errors"
 
 var (
-	ErrRequestFailed         = errors.New("remote request failed with non 200 status code")
-	ErrLocalRatelimit        = errors.New("exceeded local rate limit")
-	ErrRemoteRatelimit       = errors.New("exceeded remote rate limit")
-	ErrUnauthorizedRequest   = errors.New("unauthorized request")
-	ErrRequireAuthentication = errors.New("endpoint requires valid token")
+	ErrRequestFailed       = errors.New("remote request failed with non 200 status code")
+	ErrLocalRatelimit      = errors.New("exceeded local rate limit")
+	ErrRemoteRatelimit     = errors.New("exceeded remote rate limit")
+	ErrUnauthorizedRequest = errors.New("unauthorized request")
 )
